259c71c0b6611c6b8677776ffe39e8f921ba6520
sivaperumal12 <54525408+sivaperumal12@users.noreply.github.com>
2019-08-27 20:08:26 +0530

    Update PimgPlate.py

--- PimgPlate.py
+++ PimgPlate.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 ...........................
-class PossiblePlate:
+class PimgPlate:
     # constructor ................................
     def __init__(self):
         self.imgPlate = None
